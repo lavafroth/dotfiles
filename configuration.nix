4d3b573c2840a0e6f53e147dede50d834c9d4636
Himadri Bhattacharjee <lavafroth@protonmail.com>
2023-07-18 16:30:51 +0530

    add dnscrypt-proxy2 for encrypted dns

--- configuration.nix
+++ configuration.nix
@@ -34,10 +34,35 @@
   networking = {
     hostName = "cafe";
     networkmanager.enable = true;
-    nameservers = [
-        "1.1.1.1"
-        "9.9.9.9"
-      ];
+    nameservers = [ "127.0.0.1" "::1" ];
+    # If using dhcpcd:
+    # dhcpcd.extraConfig = "nohook resolv.conf";
+    # If using NetworkManager:
+    networkmanager.dns = "none";
+  };
+
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
+
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+
+      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+      # server_names = [ ... ];
+    };
+  };
+
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
   };
 
   # Set your time zone.
@@ -125,6 +150,7 @@
       cargo
       clippy
       dxvk
+      fd
       feroxbuster
       ffuf
       file
@@ -133,7 +159,6 @@
       gcc
       gh
       ghidra
-      radare2
       gimp
       git
       gnome.gnome-boxes
@@ -164,6 +189,7 @@
       pwntools
       python312
       qemu
+      radare2
       rust-analyzer
       rustc
       rustfmt
