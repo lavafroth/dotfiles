6b4025f6471e5e03e84b7bb814921572ca6f2461
Himadri Bhattacharjee <lavafroth@protonmail.com>
2023-12-07 18:47:18 +0530

    feat: support for secureboot

--- flake.nix
+++ flake.nix
@@ -5,23 +5,33 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.3.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, lanzaboote ... }: {
     nixosConfigurations = {
       cafe = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
-          {
+          lanzaboote.nixosModules.lanzaboote
+          ({ lib, ... }: {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.h = import ./home.nix;
 
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
+            boot.loader.systemd-boot = lib.mkForce false;
+
+            boot.lanzaboote = {
+              enable = true;
+              pkiBundle = "/etc/secureboot";
+            };
+
+          })
         ];
       };
     };
