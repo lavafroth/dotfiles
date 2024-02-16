2e0b186e347a3aa59e81ddfe20163699871a9b6e
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-02-16 18:45:02 +0530

    feat: add pylsp

--- configuration.nix
+++ configuration.nix
@@ -124,6 +124,8 @@
     packages = with pkgs;
       let pythonPackages = python311.withPackages(ps: with ps; [
         jupyter
+        python-lsp-server
+        python-lsp-ruff
         pandas
         requests
         xlrd
