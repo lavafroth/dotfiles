d0499f1748ddf8f4022a8056172d11fa012e2117
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-03-06 21:11:39 +0530

    feat: move aws related stuff under .config

---
+++ hosts/default/aws.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+{
+  home.sessionVariables = {
+    AWS_CONFIG_FILE = "${config.xdg.configHome}/aws/config";
+    AWS_SHARED_CREDENTIALS_FILE = "${config.xdg.configHome}/aws/credentials";
+  };
+}
