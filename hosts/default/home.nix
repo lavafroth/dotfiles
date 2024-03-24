87dbbf3c4b050fd82bc48ba2266e5681dc0af144
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-03-24 07:39:23 +0530

    feat: helix binding for git integration

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -55,6 +55,7 @@
         editor.idle-timeout = 0;
         keys.normal."X" = ["extend_line_up" "extend_to_line_bounds"];
         keys.select."X" = ["extend_line_up" "extend_to_line_bounds"];
+        keys.normal."C-g" = [":new" ":insert-output ${pkgs.lazygit}/bin/lazygit" ":buffer-close!" ":redraw"];
       };
     };
 
