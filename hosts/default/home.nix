83395de983184e720c0248f2ff623d5e384f05db
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-03-24 07:39:10 +0530

    feat: helix bindings to shrink per line selection

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -53,6 +53,8 @@
         editor.cursor-shape.insert = "bar";
         editor.line-number = "relative";
         editor.idle-timeout = 0;
+        keys.normal."X" = ["extend_line_up" "extend_to_line_bounds"];
+        keys.select."X" = ["extend_line_up" "extend_to_line_bounds"];
       };
     };
 
