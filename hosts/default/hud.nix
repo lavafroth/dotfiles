7118be37ce26ad02f4fbc21e68ea05f43a40f682
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-06-07 09:02:55 +0530

    feat: add ags overlay

---
+++ hosts/default/hud.nix
@@ -0,0 +1,11 @@
+{pkgs, ...}: {
+  systemd.user.services.ags-hud = {
+    Unit = {
+      Description = "ags cyberpunk hud for my cosmic epoch setup";
+    };
+    Install.WantedBy = [ "xdg-desktop-autostart.target" ];
+    Service = {
+      ExecStart = "${pkgs.ags}/bin/ags";
+    };
+  };
+}
