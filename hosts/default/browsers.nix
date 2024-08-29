d3d273b23f5270d4eae2ac0c3ddadf67c0a1f0ad
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-08-29 10:31:45 +0530

    fix: don't restore session history when librewolf crashes

--- hosts/default/browsers.nix
+++ hosts/default/browsers.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }: {
   programs.librewolf = {
     enable = true;
-    settings."widget.use-xdg-desktop-portal.file-picker" = 1;
+    settings = {
+      "widget.use-xdg-desktop-portal.file-picker" = 1;
+      "browser.sessionstore.resume_from_crash" = false;
+    };
   };
 
   # For stuff that I have to log into
