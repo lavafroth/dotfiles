af5f51685035dff0a12a9c052c46632e91b60a61
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-09-08 08:04:08 +0530

    feat: add newsflash as rss reader

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -31,6 +31,10 @@
       };
     };
 
+    packages = with pkgs; [
+      newsflash
+    ];
+
     sessionVariables = {
       CUDA_CACHE_PATH = "${config.xdg.cacheHome}/nv";
       FLAKE = "${config.xdg.userDirs.publicShare}/dotfiles";
