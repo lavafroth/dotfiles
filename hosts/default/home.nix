e39ac799041b7e69232727fc8805fcfb76040b6f
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-09-09 13:50:25 +0530

    deps: remove cachix, use substituters directly during rebuilds

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -31,11 +31,6 @@
       };
     };
 
-    packages = with pkgs; [
-      newsflash
-      cachix
-    ];
-
     sessionVariables = {
       CUDA_CACHE_PATH = "${config.xdg.cacheHome}/nv";
       FLAKE = "${config.xdg.userDirs.publicShare}/dotfiles";
