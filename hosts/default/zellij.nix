cbb4b9ffee5dde401757e5820bb43409b0832144
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-09-18 19:55:55 +0530

    feat: add a very minimal zellij layout

--- hosts/default/zellij.nix
+++ hosts/default/zellij.nix
@@ -1,11 +1,12 @@
 {
   programs.zellij = {
     enable = true;
-    # enableFishIntegration = true;
+    enableFishIntegration = true;
     settings = {
       copy_command = "wl-copy";
       simplified_ui = true;
       pane_frames = false;
     };
+    home.file.".config/zellij/layouts/minimal.kdl" = ./sources/zellij/minimal.kdl;
   };
 }
