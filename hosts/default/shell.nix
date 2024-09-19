aeedb5bd8894ed4a3b6e28dbfa42f00f3711b4a0
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-09-19 09:18:42 +0530

    feat: hel theme
    yes, ghostrunner reference, happy?

--- hosts/default/shell.nix
+++ hosts/default/shell.nix
@@ -16,24 +16,25 @@
           if not set -q VIRTUAL_ENV_DISABLE_PROMPT
               set -g VIRTUAL_ENV_DISABLE_PROMPT true
           end
-          set_color yellow
+          set_color white
           printf '%s' $USER
-          set_color normal
-          printf ' at '
 
-          set_color magenta
-          echo -n (prompt_hostname)
-          set_color normal
-          printf ' in '
+          set_color red
+          echo -ne ' at '
+          set_color white
+          printf '%s' (prompt_hostname)
+          set_color red
+          echo -ne ' in '
 
-          set_color $fish_color_cwd
-          printf '%s' (prompt_pwd)
+          set_color black -b red
+          printf ' %s ' (prompt_pwd)
           set_color normal
 
           # Line 2
           echo
+          set_color normal
           if test -n "$VIRTUAL_ENV"
-              printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+              printf "(%s) " (set_color red)(basename $VIRTUAL_ENV)(set_color normal)
           end
           printf '\uf313  \u0000'
           set_color normal
--- hosts/default/zellij.nix
+++ hosts/default/zellij.nix
@@ -1,12 +1,31 @@
 {
+  home.file.".config/zellij/layouts/minimal.kdl".source = ./sources/zellij/minimal.kdl;
   programs.zellij = {
     enable = true;
-    enableFishIntegration = true;
+    # enableFishIntegration = true;
     settings = {
       copy_command = "wl-copy";
       simplified_ui = true;
       pane_frames = false;
+      default_layout = "minimal";
+
+      themes = {
+        hel = {
+          fg = "#d14231";
+          bg = "#121212";
+          black = "#000000";
+          red = "#fc564d";
+          green = "#fc564d";
+          yellow = "#D79921";
+          blue = "#3C8588";
+          magenta = "#B16286";
+          cyan = "#689D6A";
+          white = "#FBF1C7";
+          orange = "#D65D0E";
+        };
+      };
+
+      theme = "hel";
     };
-    home.file.".config/zellij/layouts/minimal.kdl" = ./sources/zellij/minimal.kdl;
   };
 }
