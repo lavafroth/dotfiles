576a9e2f16d2de7f6158eebb8508738f3cd77305
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-11-08 13:25:31 +0530

    fix: gnome renderer fails launching gtk4 apps

--- hosts/default/home/gnome.nix
+++ hosts/default/home/gnome.nix
@@ -1,5 +1,9 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
+  # remove this after https://github.com/NixOS/nixpkgs/issues/353990 is fixed
+  home.sessionVariables = {
+    GSK_RENDERER = "gl";
+  };
 
   home.packages =
     with pkgs.gnomeExtensions;
