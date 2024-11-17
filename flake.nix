29bf57652792318871086dcaa1fbea53df8c5ca1
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-11-17 16:56:08 +0530

    deps: bump flake

--- flake.nix
+++ flake.nix
@@ -34,51 +34,65 @@
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, lanzaboote, sops-nix, nix-on-droid, nix-index-database, nixos-cosmic, stylix, ... }: {
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-    nixosConfigurations = {
-      cafe-nosecureboot = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/default/configuration.nix
-          ./cachix/nixos-cosmic.nix
-          ./cachix/cuda-maintainers.nix
-          home-manager.nixosModules.home-manager
-          stylix.nixosModules.stylix
-          nix-index-database.nixosModules.nix-index
-          { programs.nix-index-database.comma.enable = true; }
-        ];
+  outputs =
+    inputs@{
+      nixpkgs,
+      home-manager,
+      lanzaboote,
+      sops-nix,
+      nix-on-droid,
+      nix-index-database,
+      nixos-cosmic,
+      stylix,
+      ...
+    }:
+    {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+      nixosConfigurations = {
+        cafe-nosecureboot = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/default/configuration.nix
+            ./cachix/nixos-cosmic.nix
+            ./cachix/cuda-maintainers.nix
+            home-manager.nixosModules.home-manager
+            # stylix.nixosModules.stylix
+            nix-index-database.nixosModules.nix-index
+            { programs.nix-index-database.comma.enable = true; }
+          ];
+        };
+
+        cafe = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/default/configuration.nix
+            ./hosts/default/secureboot.nix
+            ./cachix/nixos-cosmic.nix
+            ./cachix/cuda-maintainers.nix
+            # ./hosts/default/stylix.nix
+            home-manager.nixosModules.home-manager
+            lanzaboote.nixosModules.lanzaboote
+            # stylix.nixosModules.stylix
+            nix-index-database.nixosModules.nix-index
+            { programs.nix-index-database.comma.enable = true; }
+          ];
+        };
+
+        rahu = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            ./hosts/rahu/configuration.nix
+            sops-nix.nixosModules.sops
+          ];
+        };
       };
 
-      cafe = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/default/configuration.nix
-          ./hosts/default/secureboot.nix
-          ./cachix/nixos-cosmic.nix
-          ./cachix/cuda-maintainers.nix
-          ./hosts/default/stylix.nix
-          home-manager.nixosModules.home-manager
-          lanzaboote.nixosModules.lanzaboote
-          stylix.nixosModules.stylix
-          nix-index-database.nixosModules.nix-index
-          { programs.nix-index-database.comma.enable = true; }
-        ];
+      nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+        modules = [ ./hosts/aqua/nix-on-droid.nix ];
       };
 
-      rahu = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/rahu/configuration.nix
-          sops-nix.nixosModules.sops
-        ];
-      };
     };
-
-    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
-      modules = [ ./hosts/aqua/nix-on-droid.nix ];
-    };
-
-  };
 }
