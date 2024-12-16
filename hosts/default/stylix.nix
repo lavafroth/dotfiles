c8f5990a469b1c134e3f68259819c52c2b299d08
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-12-16 10:17:12 +0530

    fix: stylix fonts are too aggressive with complete UIs

--- hosts/default/stylix.nix
+++ hosts/default/stylix.nix
@@ -6,22 +6,22 @@
   stylix.polarity = "dark";
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-dark.yaml";
 
-  stylix.fonts = rec {
-    monospace = {
-      package = pkgs.nerd-fonts.terminess-ttf;
-      name = "Terminess NerdFont";
-    };
+  # stylix.fonts = rec {
+  #   monospace = {
+  #     package = pkgs.nerd-fonts.terminess-ttf;
+  #     name = "Terminess NerdFont";
+  #   };
 
-    serif = monospace;
-    sansSerif = monospace;
+  #   serif = monospace;
+  #   sansSerif = monospace;
 
-    emoji = {
-      package = pkgs.noto-fonts-emoji;
-      name = "Noto Color Emoji";
-    };
-    sizes = {
-      desktop = 10;
-      terminal = 10;
-    };
-  };
+  #   emoji = {
+  #     package = pkgs.noto-fonts-emoji;
+  #     name = "Noto Color Emoji";
+  #   };
+  #   sizes = {
+  #     desktop = 10;
+  #     terminal = 10;
+  #   };
+  # };
 }
