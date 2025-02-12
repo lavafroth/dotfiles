cb92fd9f0a5060eab9f7cf7bda15bc604088be1c
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-02-12 17:25:41 +0530

    feat(rahu): replace perl system switch script with the faster binary version

--- hosts/rahu/configuration.nix
+++ hosts/rahu/configuration.nix
@@ -142,6 +142,11 @@
     shell = pkgs.fish;
   };
 
+  system.switch = {
+    enable = false;
+    enableNg = true;
+  };
+
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     git
