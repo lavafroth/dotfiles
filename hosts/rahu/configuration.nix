7dd9aeedb045782e29b8312bdcbcec9585cef257
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-03-31 11:59:03 +0530

    feat(rahu): use systemd-resolved dnssec

--- hosts/rahu/configuration.nix
+++ hosts/rahu/configuration.nix
@@ -37,7 +37,21 @@
 
   networking.hostName = "rahu";
   networking.networkmanager.enable = true;
+  networking.nameservers = [
+    "1.1.1.1#one.one.one.one"
+    "1.0.0.1#one.one.one.one"
+  ];
 
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
+    ];
+    dnsovertls = "true";
+  };
   # Time zone and locale.
   time.timeZone = "Asia/Kolkata";
   i18n.defaultLocale = "en_US.UTF-8";
