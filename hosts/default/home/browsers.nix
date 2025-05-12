1cafd02a33c91f8e3fae21ec8888e092e4d8c43b
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-05-12 07:11:41 +0530

    feat: brave is default

--- hosts/default/home/browsers.nix
+++ hosts/default/home/browsers.nix
@@ -1,18 +1,8 @@
 { pkgs, ... }:
 {
-  programs.librewolf = {
-    enable = true;
-    settings = {
-      "widget.use-xdg-desktop-portal.file-picker" = 1;
-      "browser.sessionstore.resume_from_crash" = false;
-    };
-  };
-
   home.packages = with pkgs; [
     arti
     ungoogled-chromium
-    # chromium recently messed up with pipewire stuff and rendered screen share unusable
-    # hence brave for the time being
     brave
   ];
 }
