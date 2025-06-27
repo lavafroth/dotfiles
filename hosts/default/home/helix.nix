1c8f8a46d2467ff0193443e6e4557637c3660037
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-06-27 12:09:29 +0530

    feat: prune helix config

--- hosts/default/home/helix.nix
+++ hosts/default/home/helix.nix
@@ -1,15 +1,7 @@
-{ pkgs, ... }:
 {
   programs.helix = {
     enable = true;
     settings = {
-      # theme = pkgs.lib.mkForce "solarized_dark";
-      editor.cursor-shape = {
-        insert = "bar";
-        normal = "underline";
-        select = "underline";
-      };
-      editor.line-number = "relative";
       editor.idle-timeout = 0;
       keys.normal."X" = [
         "extend_line_up"
