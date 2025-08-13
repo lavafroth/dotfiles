d3b37c5f3075962f377a9ef0065e9423b5a07d11
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-08-13 18:20:18 +0530

    trying cosmic

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -12,7 +12,7 @@
     ./nvidia.nix
     ./virtualization.nix
     # ./desktops/gnome.nix
-    ./desktops/kde.nix
+    # ./desktops/kde.nix
   ];
 
   boot = {
@@ -88,6 +88,9 @@
       excludePackages = [ pkgs.xterm ];
       desktopManager.xterm.enable = false;
     };
+
+    displayManager.cosmic-greeter.enable = true;
+    desktopManager.cosmic.enable = true;
 
     displayManager.autoLogin = {
       enable = true;
