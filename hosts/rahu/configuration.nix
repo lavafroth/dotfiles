31c55423ca70a494897bd51b25f1aad1ac76c190
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-09-14 08:33:36 +0530

    feat: photoprism mysql

--- hosts/rahu/configuration.nix
+++ hosts/rahu/configuration.nix
@@ -60,13 +60,33 @@
   services.photoprism = {
     enable = true;
     port = 2342;
+    originalsPath = "/var/lib/private/photoprism/originals";
     address = "0.0.0.0";
     passwordFile = "/run/secrets/photoprism";
     settings = {
       PHOTOPRISM_ADMIN_USER = "admin";
       PHOTOPRISM_DEFAULT_LOCALE = "en";
       PHOTOPRISM_SITE_TITLE = "rahu";
+      PHOTOPRISM_DATABASE_DRIVER = "mysql";
+      PHOTOPRISM_DATABASE_NAME = "photoprism";
+      PHOTOPRISM_DATABASE_SERVER = "/run/mysqld/mysqld.sock";
+      PHOTOPRISM_DATABASE_USER = "photoprism";
     };
+  };
+
+  services.mysql = {
+    enable = true;
+    dataDir = "/data/mysql";
+    package = pkgs.mariadb;
+    ensureDatabases = [ "photoprism" ];
+    ensureUsers = [
+      {
+        name = "photoprism";
+        ensurePermissions = {
+          "photoprism.*" = "ALL PRIVILEGES";
+        };
+      }
+    ];
   };
 
   systemd.services.create_ap = {
