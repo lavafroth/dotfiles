a4a6205634f0536e24d21bdc97cdfaea231826e2
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-12-01 08:58:35 +0530

    feat: lecture script accepts a url argument or copies from clipboard

--- hosts/default/home/default.nix
+++ hosts/default/home/default.nix
@@ -63,7 +63,8 @@
       signal-desktop-bin
       rusty-man
       (pkgs.writeShellScriptBin "lecture" ''
-        mpv --speed=1.5 --start=00:00:14 --cache-pause-wait=14 --script-opts='skipsilence-enabled=yes,skipsilence-threshold_db=-18' --vf=sub,lavfi="negate"  $(wl-paste)
+        url="$1" || "$(wl-paste)"
+        mpv --speed=1.5 --start=00:00:14 --cache-pause-wait=14 --script-opts='skipsilence-enabled=yes,skipsilence-threshold_db=-18' --vf=sub,negate "$url"
       '')
     ];
   };
