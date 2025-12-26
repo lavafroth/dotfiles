d055854d3b67dfd6fd35f727278f2d213aa93d07
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-12-26 18:40:35 +0530

    feat: early KMS for systemd-boot to display console over HDMI

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -20,10 +20,15 @@
   ];
 
   boot = {
+    # https://wiki.archlinux.org/title/Kernel_mode_setting#Early_KMS_start
+    # early KMS over HDMI
     kernelParams = [
       "quiet"
       "splash"
+      "video=HDMI-1:1920x1080@60"
     ];
+    initrd.availableKernelModules = [ "i915" ];
+
     loader = {
       systemd-boot = {
         enable = true;
