0b53dd9d0c93babc6b03ee8187c62cbcbbdc0dcf
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2026-02-11 10:20:46 +0530

    feat: script to rotate screen on kde

--- hosts/default/home/kde.nix
+++ hosts/default/home/kde.nix
@@ -8,6 +8,16 @@
     kdePackages.karousel
     notify-desktop
     klassy
+    (pkgs.writeShellScriptBin "kde-rotate" ''
+      #!/usr/bin/env sh
+      case $(${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor --json | ${pkgs.jq}/bin/jq .outputs[0].rotation) in
+          1) direction=left ;;
+          2) direction=inverted ;;
+          4) direction=right ;;
+          8) direction=none ;;
+      esac
+      ${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.eDP-1.rotation.$direction
+    '')
   ];
   home.file.".config/kglobalshortcutsrc".source = ./sources/kglobalshortcutsrc;
   home.file.".config/kwinrc".source = ./sources/kwinrc;
