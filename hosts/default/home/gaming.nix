c00b230c702c21f2acd80f8e3e9b41cbd253575f
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2026-06-05 09:18:34 +0530

    feat: exclude openldap from lutris

--- hosts/default/home/gaming.nix
+++ hosts/default/home/gaming.nix
@@ -3,6 +3,12 @@
   home.packages = with pkgs; [
     dxvk
     wine64
-    lutris
+    (lutris.override {
+      buildFHSEnv = args: pkgs.buildFHSEnv ( args // {
+        multiPkgs = envPkgs: let
+        originalPkgs = args.multiPkgs envPkgs;
+        in builtins.filter (pkg: pkg.pname or "" != "openldap") originalPkgs;
+      });
+    })
   ];
 }
