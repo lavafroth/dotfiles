da003b7063db976207eed1db0a9acb9378ee6e17
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2026-06-11 12:27:30 +0530

    cafe/setuid: use lib.mkForce directly to disable setuid

--- hosts/default/disable-setuid.nix
+++ hosts/default/disable-setuid.nix
@@ -1,18 +1,18 @@
-{ pkgs, ... }:
+{ lib, ... }:
 {
   security.sudo.enable = false;
   security.wrappers = {
-    umount.setuid = pkgs.lib.mkForce false;
-    su.setuid = pkgs.lib.mkForce false;
-    sg.setuid = pkgs.lib.mkForce false;
-    pkexec.setuid = pkgs.lib.mkForce false;
-    passwd.setuid = pkgs.lib.mkForce false;
-    newuidmap.setuid = pkgs.lib.mkForce false;
-    newgrp.setuid = pkgs.lib.mkForce false;
-    newgidmap.setuid = pkgs.lib.mkForce false;
-    mount.setuid = pkgs.lib.mkForce false;
-    fusermount3.setuid = pkgs.lib.mkForce false;
-    fusermount.setuid = pkgs.lib.mkForce false;
-    chsh.setuid = pkgs.lib.mkForce false;
+    umount.setuid = lib.mkForce false;
+    su.setuid = lib.mkForce false;
+    sg.setuid = lib.mkForce false;
+    pkexec.setuid = lib.mkForce false;
+    passwd.setuid = lib.mkForce false;
+    newuidmap.setuid = lib.mkForce false;
+    newgrp.setuid = lib.mkForce false;
+    newgidmap.setuid = lib.mkForce false;
+    mount.setuid = lib.mkForce false;
+    fusermount3.setuid = lib.mkForce false;
+    fusermount.setuid = lib.mkForce false;
+    chsh.setuid = lib.mkForce false;
   };
 }
